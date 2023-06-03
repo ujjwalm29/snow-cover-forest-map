6dbc1ce0bb1ffbb3d4712b875084c45ec4ce0acc
ujjwalm29 <ujjwalm29@vt.edu>
2023-06-03 12:26:37 +0000

    Updated test.js (added 51 lines, deleted 13 lines).

--- test.js
+++ test.js
@@ -1,15 +1,53 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var dataset = ee.Image("DLR/WSF/WSF2015/v1");
+var dataset2 = ee.Image("DLR/WSF/WSF2015/v1"),
+    modis = ee.ImageCollection("MODIS/061/MOD11A1"),
+    dataset = ee.FeatureCollection("USDOS/LSIB_SIMPLE/2017");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 
-var opacity = 0.75;
-var blackBackground = ee.Image(0);
-Map.addLayer(blackBackground, null, 'Black background', true, opacity);
+var usa = dataset.filter(ee.Filter.eq('country_co', 'US'));
 
-var visualization = {
-  min: 0,
-  max: 255,
-};
-Map.addLayer(dataset, visualization, 'Human settlement areas');
 
-Map.setCenter(90.45, 23.7, 7);
+var start = ee.Date('2015-01-01');
+var dateRange = ee.DateRange(start, start.advance(1, 'year'));
+
+var mod11a2 = modis.filterDate(dateRange);
+
+var modLSTday = mod11a2.select('LST_Day_1km');
+
+
+
+var modLSTinC = modLSTday.map(function(img) {
+  return img
+    .multiply(0.02)
+    .subtract(273.15)
+    .copyProperties(img, ['system:time_start']);
+});
+
+
+
+print(usa);
+
+Map.centerObject(usa, 6);
+Map.addLayer(usa);
+
+// var ts1 = ui.Chart.image.series({
+//   imageCollection: modLSTc,
+//   region: india,
+//   reducer: ee.Reducer.mean(),
+//   scale: 1000,
+//   xProperty: 'system:time_start'})
+//   .setOptions({
+//     title: 'LST 2015 Time Series',
+//     vAxis: {title: 'LST Celsius'}});
+// print(ts1);
+
+
+
+var clippedLSTc = modLSTinC.mean().clip(usa);
+
+Map.addLayer(clippedLSTc, {
+  min: -5, max: 30,
+  palette: ['blue', 'limegreen', 'yellow', 'darkorange', 'red']},
+  'Mean temperature, 2015');
+
+
